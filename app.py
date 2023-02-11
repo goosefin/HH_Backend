e42063f5f502836027046f898c79720e336164f1
goosefin <agustinafin96@gmail.com>
2023-02-11 10:41:08 -0500

    imported packages in app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask,jsonify
+import models
+from flask_cors import CORS
+from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
+from resources.apartment import apartments
+from resources.user import user
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
